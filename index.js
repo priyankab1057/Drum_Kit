450c3507b6b901fe33bb9afbf0913a65fcbc9d4b
Priyanka <priyankab1057@gmail.com>
2024-07-31 06:01:54 -0500

    Add files via upload
    Adding drum kit with mouse click and keypress events

---
+++ index.js
@@ -0,0 +1,99 @@
+var allButtons = document.getElementsByClassName('drum');
+
+var buttonLength = allButtons.length;
+
+for (var i = 0; i <= buttonLength; i++) {
+    allButtons[i].addEventListener('click', handleClick);
+    
+}
+
+
+function handleClick() {
+    var buttonSounds = this.innerHTML;
+
+    makeSound(buttonSounds);
+    animation(buttonSounds);
+
+    document.addEventListener('keydown', function (event) {
+        makeSound(event.key);
+        animation(event.key);
+    })
+
+    switch (buttonSounds) {
+        case 'w':
+            var tom1 = new Audio('./sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case 'a':
+            var tom2 = new Audio('./sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case 's':
+            var tom3 = new Audio('./sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case 'd':
+            var tom4 = new Audio('./sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        case 'j':
+            var crash = new Audio('./sounds/crash.mp3');
+            crash.play();
+            break;
+        case 'k':
+            var kick = new Audio('./sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        case 'l':
+            var snare = new Audio('./sounds/snare.mp3');
+            snare.play();
+            break;
+        default: console.log(buttonSounds);
+    }
+}
+
+function makeSound(key) {
+    switch (key) {
+        case 'w':
+            var tom1 = new Audio('./sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case 'a':
+            var tom2 = new Audio('./sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case 's':
+            var tom3 = new Audio('./sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case 'd':
+            var tom4 = new Audio('./sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        case 'j':
+            var crash = new Audio('./sounds/crash.mp3');
+            crash.play();
+            break;
+        case 'k':
+            var kick = new Audio('./sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        case 'l':
+            var snare = new Audio('./sounds/snare.mp3');
+            snare.play();
+            break;
+        default: console.log(buttonSounds);
+    }
+
+
+}
+    
+function animation(key) {
+
+    document.querySelector('.' + key).classList.add('pressed');
+
+    setTimeout(function () {
+        document.querySelector('.' + key).classList.remove('pressed');
+    }, 100);
+   
+}
